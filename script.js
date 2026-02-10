debfdc790b2e9e6630de107720de3a6f11eab7d2
Elainedc <diascarvalhoelaine@gmail.com>
2026-02-10 19:47:50 -0300

    onclick

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ let list = document.querySelectorAll('.item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 
-let count = list.length //3 count = contagem 
-let active = 0 //o active começa em 0 porque é o item que está ativo (0,1,2)
-console.log(count)
+let count = list.length //3 count = contagem o código vai se adaptar automaticamente
+let active = 0 //o active começa em 0 porque é o item que está  ativo no momento. quando passar pro outro lado o active vai mudando para outro item na tela. (0,1,2)
+
+//arrow function, jeito moderno de escrever funções, função do tipo arrow (flecha)
+next.onclick = () => {
+    console.log("Next")
+}
+
+prev.onclick = () => {
+    console.log("Prev")
+}
