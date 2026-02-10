a3ffd461ecfe8d5d06580395a87e44e62b439f8c
Elainedc <diascarvalhoelaine@gmail.com>
2026-02-10 19:38:54 -0300

    add javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+let list = document.querySelectorAll('.item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+
+let count = list.length //3 count = contagem 
+let active = 0 //o active começa em 0 porque é o item que está ativo (0,1,2)
+console.log(count)
